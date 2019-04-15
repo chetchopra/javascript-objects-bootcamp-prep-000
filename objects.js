c4a1a112cc4ed5cf37f2af5a8f8e035ae9032fcf
bustling-maker-4835 <chopra.chet@gmail.com>
2019-04-15 20:43:32 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,3 +3,7 @@ var playlist = {kayne: "music"}
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle
 }
+
+function removeFromPlaylist(playlist, artistName) {
+  
+}
